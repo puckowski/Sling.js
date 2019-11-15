f987ba998da97bf91d501ce990965561721a059c
whatever <puckowski.d@gmail.com>
2019-11-15 14:10:48 -0500

    Updated main.js.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import LoginCardComponent from './app/components/login-card.component.js';
 import NavbarComponent from './app/components/navbar.component.js';
 
+let state = { buttonClass: 'btn-primary', index: 0 };
+s.setState(state);
+
 let compNavbar = new NavbarComponent();
 const rendNavbar = s.render(compNavbar.view());
 let rootNavbar = s.mountById('divNavbar', rendNavbar);
@@ -9,10 +12,7 @@ let compLogin = new LoginCardComponent();
 const rendLogin = s.render(compLogin.view());
 let rootContent = s.mountById('divContent', rendLogin);
 
-let state = { buttonClass: 'btn-primary', index: 0 };
-s.setState(state);
-
-s.autoupdate(rootContent, rendLogin, compLogin);
+s.autoUpdate(rootContent, rendLogin, compLogin);
 
 setInterval(() => {
     let state = s.getState();
