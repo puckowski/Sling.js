7fbc8abb5f1bc1b1f280fa7da2e8488069290c89
whatever <puckowski.d@gmail.com>
2019-11-29 11:20:17 -0500

    Removed stream code from todo.js.

--- src/todo.js
+++ src/todo.js
@@ -32,33 +32,3 @@ let compTodoHeader = new TodoHeaderComponent();
 s.mount('divTodoHeader', compTodoHeader);
 
 s.autoUpdate('navTodoList', compTodoListNav);
-
-
-let str = s.stream();
-str.push(1);
-str.push(2);
-/*let mapFn = function(val) {
-    val = val * 2;
-}*/
-const map1 = function(array) { return array.map(x => x * 2) };
-str.log();
-str.transform(map1);
-str.log();
-str.push(3);
-const map2 = function(array) { return array.map(x => x * 2) };
-str.transform(map2);
-str.log();
-const filter1 = function(array) { return array.filter(x => x > 6) };
-str.transform(filter1);
-str.log();
-str.transform(map1);
-str.log();
-const reducer1 = function(array) { return array.reduce((accumulator, currentValue) => accumulator + currentValue); };
-str.transform(reducer1);
-str.log();
-str.transform(map1).transform(map1);
-str.log();
-str.clearTransformers();
-str.from([1, 2]);
-str.transform(map1).transform(map1);
-str.log();
